d08c191ded3485cf184471f0bc00ca0757153777
Palekov Alexander <palekov-2011@mail.ru>
2024-06-17 16:19:23 +0300

    initial commit

---
+++ src/main/java/ru/palekov/LoggingConfigurationGradle.java
@@ -0,0 +1,7 @@
+package ru.palekov;
+
+public class LoggingConfigurationGradle {
+    public static void testLog(String message) {
+        System.out.println("Message from logging-starter-gradle: " + message);
+    }
+}
